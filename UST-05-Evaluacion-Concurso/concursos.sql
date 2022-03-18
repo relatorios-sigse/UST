e4e6841d7f7f4e71aea72708dae5c1ad87167f82
andresdelrio <andresdelrio@air-de-andres.lan>
2022-03-18 13:08:37 -0400

    Nuevo campo departamento. Why: Poder filtrar checklist de admisibilidad

--- UST-05-Evaluacion-Concurso/concursos.sql
+++ UST-05-Evaluacion-Concurso/concursos.sql
@@ -2,19 +2,29 @@ SELECT
         WFP.IDPROCESS idconcurso,
         WFP.NMPROCESS nminstancia,
         CONCURSO.nomcombocatoria nmconcurso,
-        CASE WHEN CONCURSO.tipoagen = 1 THEN 'INTERNA'
-        WHEN CONCURSO.tipoagen = 2 THEN 'EXTERNA' ELSE '' END tipoconcurso,
-        CASE WHEN CONCURSO.tipoeval = 1 THEN 'INTERNA'
-        WHEN CONCURSO.tipoeval = 2 THEN 'EXTERNA'
-        WHEN CONCURSO.tipoeval = 3 THEN 'MIXTA'
-        WHEN CONCURSO.tipoeval = 4 THEN 'NO APLICA' END tipoevaluacion
+        CASE 
+            WHEN CONCURSO.tipoagen = 1 THEN 'INTERNA'   
+            WHEN CONCURSO.tipoagen = 2 THEN 'EXTERNA' 
+            ELSE '' 
+        END tipoconcurso,
+        CASE 
+            WHEN CONCURSO.tipoeval = 1 THEN 'INTERNA'   
+            WHEN CONCURSO.tipoeval = 2 THEN 'EXTERNA'   
+            WHEN CONCURSO.tipoeval = 3 THEN 'MIXTA'   
+            WHEN CONCURSO.tipoeval = 4 THEN 'NO APLICA' 
+        END tipoevaluacion,
+		AGENCIA.CTIPOAGENINTE departamento
+		
     FROM
-        WFPROCESS WFP      
+        WFPROCESS WFP           
     JOIN
-        GNASSOCFORMREG REG              
-            ON WFP.CDASSOCREG = REG.CDASSOC      
+        GNASSOCFORMREG REG                           
+            ON WFP.CDASSOCREG = REG.CDASSOC           
     JOIN
-        DYNFORM CONCURSO              
-            ON REG.OIDENTITYREG=CONCURSO.OID          
+        DYNFORM CONCURSO                           
+            ON REG.OIDENTITYREG=CONCURSO.OID    
+    LEFT JOIN
+        DYNDTIPOAGENINTE AGENCIA 
+            ON AGENCIA.OID = CONCURSO.OIDABCUJBDM2NQZG3L     
     WHERE
         CDPROCESSMODEL = 15  
